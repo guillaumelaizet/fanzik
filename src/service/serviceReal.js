0b25868a920c5e10222009e473ae3bc7b3c38f63
Guillaume Laizet <glaizet@gmail.com>
2018-12-03 12:03:01 +0100

    push changes

--- src/service/serviceReal.js
+++ src/service/serviceReal.js
@@ -79,7 +79,7 @@ class Service {
   // --------------------------------------------------------- Auth -------------------------------------------------------
 
   register (userData) {
-    const url = 'http://localhost:3000/api/register'
+    const url = 'api/register'
     let params = {
       pseudo: userData.pseudo,
       email: userData.email,
@@ -98,7 +98,7 @@ class Service {
   }
 
   login (userData) {
-    const url = 'http://localhost:3000/api/login'
+    const url = 'api/login'
     let params = {
       pseudo: userData.pseudo,
       password: userData.password
